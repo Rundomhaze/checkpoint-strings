13c5a37bd4eb6fa35624faa91b7a86891248b771
Alex <petenkov2822@gmail.com>
2022-05-23 11:31:39 +0400

    3 tests  done

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+function countVowels(str) {
+  let count = 0;
+  let arrStr = str.toLowerCase().split('')
+  for (let i = 0; i < arrStr.length; i++) {
+    if (arrStr[i] == 'а' ||
+      arrStr[i] == 'я' ||
+      arrStr[i] == 'у' ||
+      arrStr[i] == 'е' ||
+      arrStr[i] == 'и' ||
+      arrStr[i] == 'о' ||
+      arrStr[i] == 'ю' ||
+      arrStr[i] == 'э' ||
+      arrStr[i] == 'ы') {
+      count++
+    }
+  }
+  return count;
+}
+
+
+function splitWords(str) {
+  return str.match(/[а-яА-я]+/g)
+}
+
+
+
+
+function removePunctuation(str) {
+  return str.replace(/[\s.,-]/g, '');
+}
+
+
 
 module.exports = {
   countVowels,
