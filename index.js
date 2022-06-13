98bdcd4efb1c25e0c7c30a5da1f272c2683f130b
Alex <petenkov2822@gmail.com>
2022-06-13 09:43:57 +0300

    Fix regExp

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-function countVowels(str) {
-  let count = 0;
-  let arrStr = str.toLowerCase().split('')
-  for (let i = 0; i < arrStr.length; i++) {
-    if (arrStr[i] == 'а' ||
-      arrStr[i] == 'я' ||
-      arrStr[i] == 'у' ||
-      arrStr[i] == 'е' ||
-      arrStr[i] == 'и' ||
-      arrStr[i] == 'о' ||
-      arrStr[i] == 'ю' ||
-      arrStr[i] == 'э' ||
-      arrStr[i] == 'ы') {
-      count++
-    }
-  }
-  return count;
+function countVowels(string) {
+  // let count = 0;
+  // const arrayStr = string.toLowerCase().split('');
+  // for (let i = 0; i < arrayStr.length; i++) {
+  //   if (arrayStr[i] == 'а' ||
+  //     arrayStr[i] == 'я' ||
+  //     arrayStr[i] == 'у' ||
+  //     arrayStr[i] == 'е' ||
+  //     arrayStr[i] == 'и' ||
+  //     arrayStr[i] == 'о' ||
+  //     arrayStr[i] == 'ю' ||
+  //     arrayStr[i] == 'э' ||
+  //     arrayStr[i] == 'ы') {
+  //     count++
+  //   }
+  // }
+  // return count;
+
+  return string.toLowerCase().match(/[аяуеиоюэы]/g).length
+
+};
+
+function splitWords(string) {
+  return string.match(/[а-яА-Я]+/g)
+
+};
+
+function removePunctuation(string) {
+  return string.replace(/[\s.,-]/g, '')
 }
-
-
-function splitWords(str) {
-  return str.match(/[а-яА-я]+/g)
-}
-
-
-
-
-function removePunctuation(str) {
-  return str.replace(/[\s.,-]/g, '');
-}
-
 
 
 module.exports = {
